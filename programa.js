319823c5ea7156639bd31cfdea25c451fff0b21c
mrcesar95 <cesaruxfxtx@gmail.com>
2023-03-06 06:26:06 -0500

    Subiendo Taller 1: Javascript

---
+++ programa.js
@@ -0,0 +1,31 @@
+// Definir dos variables y asignarles valores
+
+var a = 4
+var b = 45
+
+// Utilizar decisiones para imprimir la variable con el mayor valor
+
+if (a > b) {
+  console.log("El número " + a + " es mayor que " + b);
+} else if (b > a) {
+  console.log("El número " + b + " es mayor que " + a);
+} else {
+  console.log("Los números son iguales");
+}
+
+
+
+// Definir tres variables y asignarles valores
+let a = 10;
+let b = 20;
+let num3 = 15;
+
+// Utilizar decisiones para imprimir la variable con el mayor valor
+if (a > b && a > num3) {
+  console.log("La variable num1 tiene el mayor valor: " + a);
+} else if (b > a && b > num3) {
+  console.log("La variable num2 tiene el mayor valor: " + b);
+} else {
+  console.log("La variable num3 tiene el mayor valor: " + num3);
+
+}
